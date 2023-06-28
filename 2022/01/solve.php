1b726e91f0a23a80421b4c2f537b1b7a40be2b11
Andrew Monty <andrew1503@gmail.com>
2023-06-28 16:44:56 -0400

    Made output more concise

--- 2022/01/solve.php
+++ 2022/01/solve.php
@@ -25,5 +25,7 @@ arsort($list);
 
 $slice = array_slice($list, 0, 3);
 
-var_dump($slice);
-var_dump(array_sum($slice));
+echo "top three elve's total calories: " . implode(', ', $slice) . "\n";
+echo "total calories across top three: " . array_sum($slice) . "\n";
+
+?>
