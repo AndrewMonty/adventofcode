30b59a2ffe13eff8be4206072a4cb17859ddd804
Andrew Monty <andrew1503@gmail.com>
2023-07-10 23:01:35 -0400

    Solved 2022 day 06

---
+++ 2022/06/solve2.php
@@ -0,0 +1,37 @@
+<?php
+
+$input = file_get_contents(__DIR__ . '/input.txt');
+
+$start = find_start_of_packet_marker($input) + 1;
+
+echo "first marker is after character $start\n";
+
+function find_start_of_packet_marker(string $string): int
+{
+    $buffer = [];
+
+    foreach (str_split($string) as $index => $char) {
+        $buffer[] = $char;
+
+        if (count($buffer) < 14) {
+            continue;
+        }
+
+        if (count($buffer) > 14) {
+            array_shift($buffer);
+        }
+
+        $bufferString = implode('', $buffer);
+
+        $uniqueString = count_chars($bufferString, 3);
+
+        if (strlen($uniqueString) == 14) {
+            // echo "$index: $char is not in $bufferString\n";
+            return $index;
+        }
+
+        // echo "$index: $char is in $bufferString\n";
+    }
+
+    return -1;
+}
